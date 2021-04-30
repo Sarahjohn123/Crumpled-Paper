95abb34f3ca6af82401c1af004edbbaf3bc66f93
Sarahjohn123 <79527270+Sarahjohn123@users.noreply.github.com>
2021-04-30 14:07:38 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,11 @@
+class paper{
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            'density':1.2,
+            'friction':0,
+            'restitution':0.8,
+
+        }
+    }
+}
